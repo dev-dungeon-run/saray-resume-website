b389271cd3cb5051dbab9957a40a8516ed995362
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-04-28 13:24:11 -0600

    started resumee page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Navbar from './shared/navbar/navbar.jsx';
 import Projects from "./pages/projects/projects.jsx";
+import Resume from "./pages/resume/resume.jsx"
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className="page-content">
         <Routes>
           <Route path="/" element={<Projects />} />
-          <Route path="/resume" element={<h1>Resume Page</h1>} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </div>
     </div>
--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -1,8 +1,40 @@
+import { Link } from "react-router-dom";
+import "./div1.css"
+import { FaEnvelope, FaPhone, FaGithub, FaLinkedin } from "react-icons/fa";
+import Navbar from "../../shared/navbar/navbar.jsx";
+
+import "./resume.css"
+
+import { useState } from "react"; // to expand card
+
 
 function Resume() {
   return (
-    <div>
-        <h1>Resume</h1>
+    <div className="resume_page">
+      <Navbar />
+      <div className="resume_container">
+        <div class="container">
+          <div class="main_div main_div1">
+            <div className="div1_1 div1">
+
+            </div>
+
+            <div className="div1_2 div1">
+
+            </div>
+          </div>
+
+
+
+          <div class="main_div main_div2">
+
+          </div>
+          <div class="main_div main_div3">
+
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
