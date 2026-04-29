a6ae4e4c8c0abffbb7b8bf164bf0db0a4f693a0d
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-04-28 21:59:01 -0600

    layput for di1 in resume

--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
 import "./div1.css"
+import "./div2.css"
+import "./resume.css"
+import "./shared.css"
+
+
 import { FaEnvelope, FaPhone, FaGithub, FaLinkedin } from "react-icons/fa";
 import Navbar from "../../shared/navbar/navbar.jsx";
+import meImg from "../../assets/resume_page_images/drawingOfMe.png"
 
-import "./resume.css"
 
 import { useState } from "react"; // to expand card
 
@@ -15,20 +20,61 @@ function Resume() {
       <div className="resume_container">
         <div class="container">
           <div class="main_div main_div1">
-            <div className="div1_1 div1">
+            <div className="div1_1 div1 clickable">
+
+              <img src={meImg} alt="" className="resume_image" />
+
+              <div className="myName_Student">
+                <h1 className="sarayMartinez">Saray Alexandra <br /> Martinez</h1>
+
+                <div className="line"></div>
+
+                <h2>Software Engineering Student</h2>
+              </div>
 
             </div>
 
             <div className="div1_2 div1">
+              <div className="div1_2_1">
+                <p> this is me ladi da</p>
+                <button className="location">
+                  Location
+                </button>
+              </div>
 
+
+
+
+
+
+              <div className="div1_2_2">
+                <div className="git_linkedin_download_butttons_container">
+                  <div className="div1_2_2_1">
+                    <button> git</button>
+                    <button>linkedin </button>
+                  </div>
+
+                  <div className="div1_2_2_2">
+                    <button>Download</button>
+                  </div>
+                </div>
+
+                <button> email</button>
+
+              </div>
             </div>
           </div>
 
-
-
+          {/* ++++++++++++ */}
+          {/* main div 2 */}
+          {/* ****************** */}
           <div class="main_div main_div2">
 
+
           </div>
+
+
+
           <div class="main_div main_div3">
 
           </div>
